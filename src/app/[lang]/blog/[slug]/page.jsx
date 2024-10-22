fca720b8d7d0b11fabf75fa2dfc0c97be9d129c7
Kizunice <dev.kizunice@gmail.com>
2024-10-22 09:59:26 +0700

    adding blog page with cms rev5

--- src/app/[lang]/blog/[slug]/page.jsx
+++ src/app/[lang]/blog/[slug]/page.jsx
@@ -37,7 +37,7 @@ const SingleBlogPage = async ({ params }) => {
         )}
       </div>
 
-      <article className="prose lg:prose-xl lg:max-w-[50rem] justify-center align-center mx-auto">
+      <article className="prose lg:prose-xl lg:max-w-[50rem] justify-center align-center mx-auto mb-8">
           <RenderBodyContent post={post} />
       </article>
     </article>
@@ -45,3 +45,55 @@ const SingleBlogPage = async ({ params }) => {
 };
 
 export default SingleBlogPage;
+
+// import { PortableText } from "next-sanity";
+// import imageUrlBuilder from "@sanity/image-url";
+// import { client } from "@/sanity/client";
+// import Link from "next/link";
+// import Image from "next/image";
+
+// const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
+
+// const { projectId, dataset } = client.config();
+// const urlFor = (source) =>
+//   projectId && dataset
+//     ? imageUrlBuilder({ projectId, dataset }).image(source)
+//     : null;
+
+// const options = { next: { revalidate: 30 } };
+
+// export default async function PostPage({
+//   params,
+// }) {
+//   const post = await client.fetch(POST_QUERY, params, options);
+//   const postImageUrl = post.image
+//     ? urlFor(post.image)?.width(550).height(310).url()
+//     : null;
+
+//   return (
+//     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
+//       <Link href="/id/blog" className="hover:underline">
+//         ← Back to posts
+//       </Link>
+     
+//       <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+//       <div className="relative z-0 mx-auto aspect-video max-w-screen-lg overflow-hidden lg:rounded-lg mb-4">
+//         {postImageUrl && (
+//           <Image
+//             priority
+//             src={postImageUrl}
+//             alt={post.title || "Thumbnail"}
+//             loading="eager"
+//             fill
+//             sizes="100vw"
+//             className="object-cover rounded-md"
+//           />
+//         )}
+//       </div>
+//       <div className="prose">
+//         <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
+//         {Array.isArray(post.body) && <PortableText value={post.body} />}
+//       </div>
+//     </main>
+//   );
+// }
--- src/components/ui/BlogItem.jsx
+++ src/components/ui/BlogItem.jsx
@@ -1,19 +1,35 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from "next/image";
+import { urlForImage } from "@/sanity/image";
 
 const BlogItem = ({blog}) => {
-  return (
-    <Link href={`/id/blog/${blog.slug.current}`} className='block p-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 my-8'>
-      
-      <article>
-        <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-700">{blog.title}</h3>
-        <p className='mb-3 font-normal text-sm text-gray-600'>{new Date(blog.publishedAt).toDateString()}</p>
+    const imageProps = blog.mainImage
+    ? urlForImage(blog.mainImage)
+    : null;
+    return (
+        <Link href={`/id/blog/${blog.slug.current}`} className='block p-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 my-8'>
+        
+        <article>
+            {imageProps && (
+            <Image
+                priority
+                src={imageProps.src}
+                alt={blog.mainImage.alt || "Thumbnail"}
+                loading="eager"
+                fill
+                sizes="100vw"
+                className="object-cover rounded-md"
+            />
+            )}
+            <h3 className="mb-auto pb-4 text-xl font-bold tracking-tight text-gray-700">{blog.title}</h3>
+            <p className='b-0 font-normal text-sm text-gray-600'>{new Date(blog.publishedAt).toDateString()}</p>
 
-        {/* <p className="mb-3 font-normal text-gray-600">{blog.metadata.slice(0, 140)}...</p> */}
-      </article>
-    
-    </Link>
-  )
+            {/* <p className="mb-3 font-normal text-gray-600">{blog.metadata.slice(0, 140)}...</p> */}
+        </article>
+        
+        </Link>
+    )
 }
 
 export default BlogItem
